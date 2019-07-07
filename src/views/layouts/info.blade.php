ffb8e4fd306e280b009b037a1bd13caaa33a00b0
Abu Bakar Siddique <absiddique.live@gmail.com>
2019-07-07 19:32:41 +0600

    [View] Admin LTE 3.0.* beta added

---
+++ src/views/layouts/info.blade.php
@@ -0,0 +1,65 @@
+@extends('easy-crud::layouts.app')
+
+@section('content')
+    <div class="col-12">
+        <div class="card card-default">
+            <div class="card-header">
+                <h3 class="card-title text-capitalize">
+                    {{--<i class="fas fa-bullhorn"></i>--}}
+                    @yield('blockTitle')
+                    @if (Route::currentRouteName() != 'slider.view' && $isCreatable)
+                        |
+                        <small>
+                            <a href="{{ route("{$routePrefix}.create") }}" class="btn btn-sm btn-info">
+                                <i class="fas fa-plus" aria-hidden="true"></i>
+                                Add New
+                            </a>
+                        </small>
+                    @endif
+                </h3>
+                
+                @if(preg_match("/\.index/i", Route::currentRouteName()))
+                    <div class="card-tools">
+                        <form method="get" class="form-inline">
+                            @if ($dateSearch??null)
+                                <div class="input-group date">
+                                    <div class="input-group-addon">
+                                        <i class="fa fa-calendar"></i>
+                                    </div>
+                                    <input type="text" name="date" class="form-control pull-right" id="datepicker">
+                                    
+                                    @push("script")
+                                        <link href="{{ asset("plugins/datepicker/datepicker3.css") }}"
+                                              rel="stylesheet" />
+                                        <script src="{{ asset("plugins/datepicker/bootstrap-datepicker.js") }}"></script>
+                                        <script type="text/javascript">
+                                            $(function () {
+                                                //Date picker
+                                                $('#datepicker').datepicker({
+                                                    autoclose: true,
+                                                    format: "yyyy-mm-dd"
+                                                })
+                                            });
+                                        </script>
+                                    @endpush
+                                </div>
+                            @endif
+                            
+                            <div class="input-group">
+                                <input class="form-control" type="text" name="query" id="query"
+                                       placeholder="Keywords" />
+                                <div class="input-group-append">
+                                    <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                @endif
+            </div>
+            
+            @include('partials.action_notification')
+            @yield('dataBlock')
+        </div>
+        <!-- /.card -->
+    </div>
+@endsection
